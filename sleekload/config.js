1dba536ba5b1b5383c2539674786184fa907be30
ShakesW <gameboycrafter@gmail.com>
2022-06-27 17:54:00 -0400

    Update config.js

--- sleekload/config.js
+++ sleekload/config.js
@@ -109,11 +109,11 @@ config.background_darkening = 30
 
 // Play music during the loading screen?
 // "true" or "false"
-config.music_enable = false
+config.music_enable = true
 
 // Display current music name?
 // "true" or "false"
-config.music_show = true
+config.music_show = false
 
 // Music playlist
 // Add as many youtube ids/urls or audio files you want
@@ -122,20 +122,8 @@ config.music_show = true
 // (Place the audio files in the music folder)
 config.music_playlist = [
 	{
-		name: "Jason Shaw - Sideways Samba - Audionautix.com",
-		file: "sideways_samba.ogg",
-	},
-	{
-		name: "Devil Survivor 2 - Exploration",
-		youtube: "https://www.youtube.com/watch?v=Zyu-BSdma3g",
-	},
-	{
-		name: "VA-11 Hall-A - Every Day Is Night",
-		youtube: "https://youtu.be/H-AfGh8gmiQ",
-	},
-	{
-		name: "Sonic Mania - Comfort Zone",
-		youtube: "f4CBQ9nnQSY",
+		name: "the Office Complex",
+		youtube: "https://www.youtube.com/watch?v=87Ew0HkqWfo",
 	},
 ]
 
@@ -145,10 +133,10 @@ config.music_random_order = true
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-config.music_volume = 15
+config.music_volume = 1
 
 // Use Gmod volume for the music instead of manual volume
-config.music_gmod_volume = true
+config.music_gmod_volume = false
 
 // Enable custom messages?
 // "true" or "false"
@@ -160,19 +148,27 @@ config.messages_list = [
 	"Testing...testing... Everything seems to be in order.",
 	"Uh...it's probably not a problem...probably...but I'm showing a small discrepancy in...well, no, it's well within acceptable bounds again. Sustaining sequence.",
 	"We've boosted the Anti-Mass Spectrometer to 105 percent. Bit of a gamble, but we need the extra resolution.",
+	"I suspected this could happen but the Administrator just would not listen.",
+	"Do you suspect some alien subterfuge behind this failure?",
+	"Are you thinking what I'm thinking?",
+	"I've told you a hundred times, I am opposed to pushing this equipment beyond the test level.",
+	"It's much too unpredictable. Don't let it overcharge!",
+	"Nature made them. We were only studying them.",
+	"Put that thing down! Why must you security guards always insist on playing with our prototypes?",
+	"Do you think that, perhaps, I could have a crowbar of my own? Perhaps, then, an automatic rifle?",
 ]
 
 // Random message order?
 // "true" or "false"
-config.messages_random_order = false
+config.messages_random_order = true
 
 // Delay between message changes
 // in milliseconds
-config.messages_delay = 5000
+config.messages_delay = 10000
 
 // Messages fade duration
 // in milliseconds
-config.messages_fade_duration = 1000
+config.messages_fade_duration = 3000
 
 // Show errors inside Garry's Mod?
 // Can be useful for finding errors
